71fa8f5efde6c270e7db6e1ca934fc78eeb961fc
Bryan Jerger <Bjerger1470@gmail.com>
2019-12-05 23:33:02 -0600

    push 4 fixed all js to run

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 //api key:062c7dca4b324ae41a41ee73c66e4fce
 
 //Elements
-const iconElement = document.getElementById(".weather-icon")
-const tempElement = document.getElementById(".temperature-value")
-const descElement = document.getElementById(".temperature-description")
-const locationElement = document.getElementById(".location")
-const notificationElement = document.getElementById(".notification")
+const iconElement = document.getElementById("weather-icon")
+const tempElement = document.getElementById("temperature-value")
+const descElement = document.getElementById("temperature-description")
+const locationElement = document.getElementById("location")
 const buttonElement = document.getElementById("submit")
 const inputValue = document.getElementById("inputValue")
 const locationElementS = document.getElementById("locationS")
@@ -28,13 +27,15 @@ const KELVIN = 273;
 //get local position
 
 window.addEventListener("load",() => {
-    let lat; 
-    let lon; 
+    
 
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition(setPosition => {
-            lon = setPosition.coords.longitude;
-            lat = setPosition.coords.latitude;
+             
+           let lon = setPosition.coords.longitude;
+           let lat = setPosition.coords.latitude;
+
+           getWeather(lat, lon);
         });
     } 
 });
@@ -43,34 +44,7 @@ window.addEventListener("load",() => {
 //local weather from api
 
 function getWeather(lat, lon){
-    fetch (`https://api.openweathermap.org/data/2.5/weather?lat=`+ {lat} + `&lon=`+ {lon} +`&appid=062c7dca4b324ae41a41ee73c66e4fce`)
-        .then(response => response.json())
-        .then(data => console.log(data))
-        
-        .then(function(data){
-            weather.temprature.value = Math.floor(data.main.temp - KELVIN);
-            weather.description = data.weather[0].description;
-            weather.iconId = data.weather[0].icon;
-            weather.city = data.name;
-            weather.country = data.sys.country;
-        })
-        .then(function(){
-            displayWeather();
-        });
-}
-
-function displayWeather(){
-    iconElement.innerHTML = `<img src= "assets/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temprature.value}° <span>C</span>`;
-    descElement.innerHTML = weather.description;
-    locationElement.innerHTML = `${weather.city}, ${weather.country}`;
-
-}
-
-//selected location
-
-   buttonElement.addEventListener('click', function(){
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=062c7dca4b324ae41a41ee73c66e4fce')
+    fetch (`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=062c7dca4b324ae41a41ee73c66e4fce`)
     .then(response => response.json())
     .then(data => {
         var iconValue = data.weather[0].icon;
@@ -78,10 +52,31 @@ function displayWeather(){
         var tempValue = Math.floor(data.main.temp - KELVIN);
         var descValue = data['weather'][0]['description'];
 
-        locationElementS.innerHTML = locationValue;
-        tempElementS.innerHTML = tempValue;
-        descElementS.innerHTML = descValue;
-        iconElementS.innerHTML = `<img src= "assets/${iconValue}.png"/>`;
+        locationElement.innerHTML = locationValue;
+        tempElement.innerHTML = tempValue;
+        descElement.innerHTML = descValue;
+        iconElement.innerHTML = `<img src= "assets/${iconValue}.png"/>`;
+        })
+        
+}
+
+
+
+//selected location
+
+   buttonElement.addEventListener('click', function(){
+    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + '&appid=062c7dca4b324ae41a41ee73c66e4fce')
+    .then(response => response.json())
+    .then(data => {
+        var iconValueS = data.weather[0].icon;
+        var locationValueS = data.name;
+        var tempValueS = Math.floor(data.main.temp - KELVIN);
+        var descValueS = data['weather'][0]['description'];
+
+        locationElementS.innerHTML = locationValueS;
+        tempElementS.innerHTML = tempValueS;
+        descElementS.innerHTML = descValueS;
+        iconElementS.innerHTML = `<img src= "assets/${iconValueS}.png"/>`;
     })
     
 .catch(err => alert("Invalid City Name"))
